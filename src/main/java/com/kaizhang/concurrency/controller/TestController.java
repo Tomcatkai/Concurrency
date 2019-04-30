c33176c8fb4181dd5e3ffb0d2630f1aedb5cf7ea
Tomcatkai <tomcatkai@gmail.com>
2019-04-30 10:10:35 +0800

    Concurrency test

--- src/main/java/com/kaizhang/concurrency/controller/TestController.java
+++ src/main/java/com/kaizhang/concurrency/controller/TestController.java
@@ -1,6 +1,5 @@
 package com.kaizhang.concurrency.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 
 @RestController
-@Slf4j
 public class TestController {
 
     @RequestMapping("/test")
